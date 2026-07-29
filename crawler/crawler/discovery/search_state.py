8d96f8821bf961955593fd34c1d244e8466cd650
Oleksandr <goblinxxx1@gmail.com>
2026-07-29 07:57:34 +0300

    feat(crawler): add independent searxng_cursor to SearchState

--- crawler/crawler/discovery/search_state.py
+++ crawler/crawler/discovery/search_state.py
@@ -9,7 +9,7 @@ from crawler.models import SourceCandidate
 log = logging.getLogger(__name__)
 
 _EMPTY = {"version": 1, "cursor": 0, "grid_cursor": 0, "site_cursor": 0,
-          "approved_cursor": 0,
+          "approved_cursor": 0, "searxng_cursor": -1,
           "next_allowed_at": 0.0, "backends": {}, "cache": {}}
 
 
@@ -53,6 +53,15 @@ class SearchState:
 
     def set_grid_cursor(self, value: int) -> None:
         self._data["grid_cursor"] = int(value)
+        self._save()
+
+    # --- searxng rotation cursor (independent of grid_cursor; -1 = unseeded -> offset at read) ---
+    @property
+    def searxng_cursor(self) -> int:
+        return int(self._data.get("searxng_cursor", -1))
+
+    def set_searxng_cursor(self, value: int) -> None:
+        self._data["searxng_cursor"] = int(value)
         self._save()
 
     # --- site-query rotation cursor (separate from grid/backend cursors) ---
